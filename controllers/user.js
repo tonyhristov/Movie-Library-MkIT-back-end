a2c55b7560cb0a6350a4b0999bf49bb0025e19d9
Antonio Hristov <tonyhristow@gmail.com>
2021-08-22 16:53:35 +0300

    feat: login

--- controllers/user.js
+++ controllers/user.js
@@ -13,6 +13,7 @@ module.exports = {
    post: {
       register: (req, res, next) => {
          const { username, password } = req.body
+
          models.User.create({ username, password })
             .then((createdUser) => {
                const token = utils.jwt.createToken({ id: createdUser._id })
@@ -21,6 +22,24 @@ module.exports = {
             .catch((err) => {
                console.log(err)
             })
+      },
+
+      login: (req, res, next) => {
+         const { username, password } = req.body
+
+         models.User.findOne({ username })
+            .then((user) => Promise.all([user, user.matchPassword(password)]))
+            .then(([user, match]) => {
+               if (!match) {
+                  res.status(401).send('Invalid password')
+               }
+
+               const token = utils.jwt.createToken({
+                  id: user._id,
+               })
+               res.header('Authorization', token).send(user)
+            })
+            .catch(next)
       },
    },
 }
